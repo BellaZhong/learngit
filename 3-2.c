d0985abb3aa4123e041c5eface87956d76286a24
Bella <906772411@qq.com>
2015-10-31 11:17:46 +0800

    init commit

---
+++ 3-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    char string[80];
+    printf("please input your name:\n");
+    scanf("%s",string);
+    printf("hello:");
+    printf("%s\n",string);
+    return 0;
+}
